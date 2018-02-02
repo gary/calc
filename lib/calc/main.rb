e46a1984926282152fa5878ba515a6266260ee79
Gary Iams <ge.iams@gmail.com>
2018-02-01 20:16:33 -0500

    Add acceptance tests for simple addition

--- lib/calc/main.rb
+++ lib/calc/main.rb
@@ -10,20 +10,36 @@ module Calc
   # Collaborates with all of the necessary objects in the system to
   # facilitate calculation.
   class Main
-    attr_reader :argv, :stdout
-    private :argv, :stdout
+    attr_reader :argv, :calculator, :parser, :stdin, :stdout
+    private :argv, :calculator, :parser, :stdin, :stdout
 
     # @param argv [Array] arguments passed to program
+    # @param stdin [IO] the standard input
     # @param stdout [IO] the standard output
-    def initialize(argv, stdout: STDOUT)
-      @argv   = argv
-      @stdout = stdout
+    def initialize(argv, stdin: STDIN, stdout: STDOUT)
+      @argv       = argv
+      @stdin      = stdin
+      @stdout     = stdout
+
+      @calculator = Calculator.new(operations: Operations.basic)
+                              .extend(ResultFormatter)
+      @parser     = Parser.new(whitelist: Operations.basic.map(&:sign))
     end
 
     # @api
     # Entry point for execution
     def execute!
-      stdout.puts 'fail'
+      calculation = argv.shift
+
+      File.open(calculation, 'r') do |file|
+        file.each_line do |items|
+          results = parser.prepare(input: items)
+
+          results.each do |result|
+            stdout.puts calculator.process(input: result)
+          end
+        end
+      end
     end
   end
 end
--- spec/features/program_calculates_single_item_input_spec.rb
+++ spec/features/program_calculates_single_item_input_spec.rb
@@ -5,11 +5,56 @@ require 'support/aruba'
 
 RSpec.describe 'Calc calculates single item input', type: :aruba do
   before do
-    run('calc')
+    write_file(transaction, items)
+    run("calc #{transaction}")
     stop_all_commands
   end
 
-  it 'fails' do
-    expect(sanitize_text(last_command_started.output)).to eq 'fail'
+  let(:transaction) { 'transaction.txt' }
+
+  context 'when the input contains only whole numbers' do
+    let(:items) do
+      <<~TRANSACTION
+        2
+        2
+        +
+      TRANSACTION
+    end
+    let(:expected_result) do
+      <<~OUTPUT
+        2
+        2
+        4
+      OUTPUT
+    end
+
+    it 'calculates the sum' do
+      expect(last_command_started.output).to eq expected_result
+    end
+  end
+
+  context 'when the input contains a mix of whole and decimal numbers' do
+    let(:items) do
+      <<~TRANSACTION
+        5.748
+        10.2
+        +
+        50.37
+        +
+      TRANSACTION
+    end
+    let(:expected_result) do
+      <<~OUTPUT
+        5.748
+        10.2
+        15.948
+        50.37
+        66.318
+      OUTPUT
+    end
+
+    it 'calculates the sum' do
+      expect(last_command_started.output).to eq expected_result
+    end
   end
 end
